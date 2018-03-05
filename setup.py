f1160e7144a43030d499633a2638948401986ab7
Vivien Maisonneuve <vivien@sqreen.io>
2018-03-05 18:12:21 +0100

    Remove incorrect PyPI classifiers in setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,6 @@ KEYWORDS = []
 CLASSIFIERS = [
     'Intended Audience :: Developers',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
@@ -54,6 +52,6 @@ setup(
     package_dir={'': 'src'},
     packages=find_packages('src'),
     install_requires=INSTALL_REQUIRES,
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
+    python_requires='>=3.4, <4',
     zip_safe=False,
 )
